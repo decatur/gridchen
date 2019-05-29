226204e692a36fb72813e9f6aad981415c2543c9
wolfgang kuehn <wolfgang-kuehn@decatur.de>
2019-05-29 18:02:11 +0200

    Pass view to component

--- GridChen.d.ts
+++ GridChen.d.ts
@@ -1,22 +1,32 @@
-declare module Bantam {
+declare module GridChen {
     export interface ISelection {
         readonly min: {rowIndex: number, colIndex: number};
         readonly max: {rowIndex: number, colIndex: number};
     }
 
-    export interface ISchema {
+    export interface StringConverter {
+        fromString: (a: string) => (number | Date | string | boolean);
+        toString: (a: (number | Date | string | boolean)) => string;
+    }
+
+    export interface IColumnSchema {
         type: string;
         title: string;
         width: number;
         fractionDigits?: number;
-        sort?: number;
-        parse?: (a: string) => (number | Date | string | boolean);
-        format?: (a: (number | Date | string | boolean)) => string;
+        sortDirection?: number;
+        converter?: StringConverter;
+        frequency?: String;
+    }
+
+    export interface IGridSchema {
+        title: string;
+        columnSchemas: IColumnSchema[];
     }
 
     export interface IPosition {
-        rowIndex: number;
-        colIndex: number;
+        row: number;
+        col: number;
     }
 
     /**
@@ -25,5 +35,24 @@ declare module Bantam {
     export interface IInterval {
         min: number;
         sup: number;
+    }
+
+    export interface IRectangle {
+        row: IInterval;
+        col: IInterval;
+    }
+
+    export type onDataChangeCallback = () => null;
+
+    export interface DataView {
+        rowCount: () => number;
+        getRows: (firstRow: number, lastRow: number) => number[][];
+        deleteRow: (rowIndex: number) => number;
+        setCell: (rowIndex: number, colIndex: number, value: number) => number;
+        insertRowBefore: (rowIndex: number) => number;
+        copy: (selection: IRectangle, sep: string) => string;
+        paste: (topRowIndex: number, topColIndex: number, matrix: number[][]) => number;
+        sort: (colIndex: number) => number;
+        plot: () => void;
     }
 }

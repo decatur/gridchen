5f8e7b0935c44042442b5db20ec54e602cbb397b
Wolfgang Kühn <wolfgang-kuehn@decatur.de>
2019-09-22 21:28:46 +0200

    shared html in demos

--- demo.js
+++ demo.js
@@ -2,14 +2,39 @@ import {createView} from "./grid-chen/DataViews.js"
 import {FullDate, FullDateConverter, DatePartialTime, DatePartialTimeConverter} from "./grid-chen/converter.js"
 
 export function createInteractiveDemoGrid(container, schema, data) {
+    function html(html) {
+        container.innerHTML = html;
+    }
+
+    html`
+    <label>
+        Edit JSON Schema
+        <textarea class="schema"></textarea>
+    </label>
+    <label>
+        Edit JavaScript Matrix
+        <textarea class="data"></textarea>
+    </label>
+    <label class="grid">
+        Grid
+        <grid-chen></grid-chen>
+    </label>
+    <label class="patch">
+        JSON Patch <button>Clear</button>
+        <textarea readonly></textarea>
+    </label>`;
+
     const schemaElement = container.querySelector('.schema');
     const dataElement = container.querySelector('.data');
+    const patchElement = document.querySelector('.patch textarea');
     const gridElement = container.querySelector('grid-chen');
     let view;
 
+    document.querySelector('.patch button').onclick = () => patchElement.value = '';
+
     function dataChanged(patch) {
         dataElement.value = REPR.stringify(view.getModel(), null, 2);
-        container.querySelector('.patch').textContent = REPR.stringify(patch, null, 2);
+        patchElement.value = REPR.stringify(patch, null, 2);
     }
 
     function resetHandler() {
@@ -35,7 +60,7 @@ export function createInteractiveDemoGrid(container, schema, data) {
             return createView(schema, data);
         }
 
-        container.querySelector('.patch').textContent = '';
+        patchElement.value = '';
         view = newView();
         gridElement.resetFromView(view);
     }
@@ -68,13 +93,14 @@ export const REPR = {
      * Return a string containing a printable representation of an object.
      * @param {*} v
      * @param {null} replacer
-     * @param {number} depth
+     * @param {number?} depth
+     * @param {number?} level
      * @returns {string}
      */
     stringify(v, replacer, depth, level) {
         level = level || 0;
         depth = depth || 0;
-        const nl0 = '\n' + Array.from({length: level*depth}, () => '  ').join('');
+        const nl0 = '\n' + Array.from({length: level * depth}, () => '  ').join('');
         const nl1 = nl0 + Array.from({length: depth}, () => '  ').join('');
         const out = [];
         if (v == null) {

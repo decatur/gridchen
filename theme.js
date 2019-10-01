93178d50d7ade3170e9f2b497b71850eefcd0dd1
Wolfgang Kühn <wolfgang-kuehn@decatur.de>
2019-10-01 20:50:46 +0200

    support dark theme

--- theme.js
+++ theme.js
@@ -1,15 +1,50 @@
-// We avoid a https://en.wikipedia.org/wiki/Flash_of_unstyled_content
-let theme;
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) theme = 'dark';
-if (window.localStorage.getItem('theme')) theme = window.localStorage.getItem('theme');
+(function () {
 
-if (theme === 'dark') {
-    let styleSheet = document.createElement('style');
-    styleSheet.textContent = `
-        body {
-            background-color: rgb(5, 5, 5);
-            color: rgb(200, 250, 250);
+    function applyScheme() {
+        // We avoid a https://en.wikipedia.org/wiki/Flash_of_unstyled_content
+        let colorScheme = window.localStorage.getItem('colorScheme') || 'preferred';
+
+        if (colorScheme === 'preferred' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            colorScheme = 'dark';
         }
-    `;
-    document.head.appendChild(styleSheet);
-}
+
+        let style;
+        if (colorScheme === 'dark') {
+            style = `
+                body, option {
+                    background-color: rgb(5, 5, 5);
+                    color: rgb(200, 250, 250);
+                }
+            `;
+        } else {
+            style = `
+                body, option {
+                    color: rgb(5, 5, 5);
+                    background-color: rgb(250, 250, 250);
+                }
+            `;
+        }
+
+        let styleSheet = document.createElement('style');
+        styleSheet.textContent = style;
+        document.head.appendChild(styleSheet);
+    }
+
+    if (!document.body) {
+        applyScheme();
+    }
+
+    window.onload = function () {
+        const colorSchemeElement = /** @type {HTMLSelectElement} */ document.getElementById('colorScheme');
+        if (!colorSchemeElement) return
+        const colorScheme = window.localStorage.getItem('colorScheme') || 'preferred';
+
+        colorSchemeElement.namedItem(colorScheme).selected = true;
+        colorSchemeElement.onchange = function () {
+            window.localStorage.setItem('colorScheme', colorSchemeElement.value);
+            applyScheme();
+        }
+    };
+
+})();
+

376e0a43db4ff8f314648b91211bbbf77d635beb
Wolfgang Kühn <wolfgang-kuehn@decatur.de>
2019-12-10 14:46:17 +0100

    diff equal

--- tests/grid-chen/utils.js
+++ tests/grid-chen/utils.js
@@ -61,34 +61,56 @@ export function scope(msg, func) {
     scopePrefix = '';
 }
 
-function error(a, b) {
+function error(a, b, msg) {
     const err = Error('assertEqual failed');
     console.error(err);
+    console.log(msg);
     console.log('Expected ' + a);
     console.log('Actual ' + b);
     throw err;
 }
 
-function stringify(o) {
-    return JSON.stringify(o, function(key, value) { return typeof value === 'number' && isNaN(value)?'NaN':value; })
+function assertEqualAtomic(a, b, msg) {
+    if (a !== b) {
+        error(a, b, msg);
+    }
 }
 
-function assertEqual(a, b) {
-
-    if (a instanceof Date && b instanceof Date) {
+/**
+ * @param {any} a 
+ * @param {any} b
+ * @param {string} [path]
+ */
+function assertEqual(a, b, path) {
+    path = path || '';
+    //console.log(path);
+    assertEqualAtomic(typeof a, typeof b, path);
+    assertEqualAtomic(Array.isArray(a), Array.isArray(b), path);
+    if (Array.isArray(a)) {
+        assertEqualAtomic(a.length, b.length, path);
+        for (let i = 0; i < a.length; i++) {
+            assertEqual(a[i], b[i], path + '/' + i);
+        }
+    } else if (a instanceof Date) {
         if (isNaN(a.getTime()) && isNaN(b.getTime())) {
             // pass
         } else if (a.getTime() !== b.getTime()) {
-            error(a, b);
+            error(a, b, path);
         }
-    } else if (typeof a === 'object' && typeof b === 'object') {
-        if (stringify(a) !== stringify(b)) {
-            error(stringify(a), stringify(b));
+    } else if (typeof a === 'object') {
+        if (a == null && b == null) {
+            // pass
+            return
+        }
+        if (a == null || b == null) {
+            error(a, b, path);
+        }
+        assertEqualAtomic(Object.keys(a).sort().join(), Object.keys(b).sort().join(), 'Keys mismatch at path ' + path);
+        for (let key in a) {
+            assertEqual(a[key], b[key], path + '/' + key);
         }
     } else {
-        if (a !== b) {
-            error(a, b);
-        }
+        assertEqualAtomic(a, b, path);
     }
 }
 

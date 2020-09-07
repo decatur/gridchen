3f551a7d6e900bc346e4e92bf49be349111597ab
Wolfgang Kühn <wolfgang-kuehn@decatur.de>
2020-09-07 12:55:55 +0200

    intro stage

--- gridchen/json_patch_merge.js
+++ gridchen/json_patch_merge.js
@@ -1,8 +1,121 @@
 /**
+ * Merges all operations with spurious, intermittened values.
+ * With two adjacent operations other + op with op.path is prefix of other.path
+ * 1) replace A + replace B -> replace B
+ * 2) replace A + remove -> remove
+ * 3) add A + replace B -> add B
+ * 4) add A + remove -> NoOp
+ *
  * @param {GridChenNS.JSONPatchOperation[]} patch
  * @returns {GridChenNS.JSONPatchOperation[]}
  */
 export function mergePatch(patch) {
+    const normalizedPatch = [];
+    for (const op of patch) {
+        if (op.op === 'replace') {
+            normalizedPatch.push({op: 'remove', path: op.path});
+            normalizedPatch.push({op: 'add', path: op.path, value: op.value});
+        } else {
+            normalizedPatch.push(op);
+        }
+    }
+    const root = {children: null, op: null};
+
+    /**
+     * @param {GridChenNS.JSONPatchOperation} op
+     */
+    function mergeOperation(op) {
+        const path = op.path.split('/').map(key => /^\d+$/.test(key) ? parseInt(key) : key);
+        path.shift();
+        let o = root;
+
+        while (path.length > 0) {
+            if (!o.children) {
+                if (Number.isInteger(path[0])) {
+                    o.children = [];
+                } else {
+                    o.children = {};
+                }
+            }
+            if (o.children[path[0]] === undefined) {
+                o.children[path[0]] = {};
+            }
+            o = o.children[path[0]];
+            path.shift();
+        }
+
+        if (op.op === 'add') {
+            if (o.op === 'remove') {
+                o.op = 'replace';
+            } else {
+                console.assert(o.op === undefined);
+                o.op = 'add';
+            }
+            o.value = op.value;
+        } else if (op.op === 'remove') {
+            if (o.op === 'add') {
+                delete o.op;
+            } else {
+                console.assert(o.op === undefined);
+                o.op = 'remove';
+            }
+            delete o.value;
+            delete o.children;
+        }
+    }
+
+    const mergedPatch = [];
+
+    function serialize(o, path) {
+        if (o.op === 'add' || o.op === 'replace') {
+            mergedPatch.push({op: o.op, path: path, value: o.value});
+            // serialize(o.children[key], path + '/' + key);
+        } else if (o.op === 'remove') {
+            mergedPatch.push({op: o.op, path: path});
+        }
+
+        for (const key in o.children) {
+            serialize(o.children[key], path + '/' + key);
+        }
+    }
+
+    for (let op of normalizedPatch) {
+        mergeOperation(Object.assign({}, op));
+    }
+
+    serialize(root, '');
+    return mergedPatch
+}
+
+/**
+ * Merges all operations with spurious, intermittened values.
+ * With two adjacent operations other + op with op.path is prefix of other.path
+ * 1) replace A + replace B -> replace B
+ * 2) replace A + remove -> remove
+ * 3) add A + replace B -> add B
+ * 4) add A + remove -> NoOp
+ *
+ * @param {GridChenNS.JSONPatchOperation[]} patch
+ * @returns {GridChenNS.JSONPatchOperation[]}
+ */
+export function mergePatch1(patch) {
+    const normalizedPatch = [];
+    for (const op of patch) {
+        if (op.op === 'replace') {
+            normalizedPatch.push({op: 'remove', path: op.path});
+            normalizedPatch.push({op: 'add', path: op.path, value: op.value});
+        } else {
+            normalizedPatch.push(op);
+        }
+    }
+    const mergedPatch = [];
+
+    /**
+     * Returns true if path starts with prefix
+     * @param {(string|int)[]} path
+     * @param {(string|int)[]} prefix
+     * @returns {boolean}
+     */
     function isPrefix(path, prefix) {
         if (prefix.length > path.length) return false;
         for (const [index, key] of prefix.entries()) {
@@ -14,12 +127,12 @@ export function mergePatch(patch) {
     /**
      * @param {GridChenNS.JSONPatchOperation} op
      */
-    function normalizeOperation(op) {
-        op.path = op.path.split('/').map(key => /^\d+$/.test(key)?parseInt(key):key);
+    function mergeOperation(op) {
+        op.path = op.path.split('/').map(key => /^\d+$/.test(key) ? parseInt(key) : key);
         let removeOps = [];
         let addOp = op;
 
-        for (let other of normalizedPatch) {
+        for (let other of mergedPatch) {
             if (op.op === 'add') {
                 for (const [index, key] of op.path.entries()) {
                     if (index > other.path.length) break;
@@ -32,8 +145,24 @@ export function mergePatch(patch) {
                 }
             } else if (op.op === 'remove') {
                 if (isPrefix(other.path, op.path)) {
-                    removeOps.push(other);
-                    addOp = undefined;
+                    if (other.op === 'replace') {
+                        console.assert(false)
+                        // 2) replace A + remove -> remove
+                        other.op = op.op;
+                        other.path = op.path;
+                        delete other.value;
+                        addOp = undefined;
+                    } else if (other.op === 'add') {
+                        // 4) add A + remove -> NoOp or remove
+                        addOp = undefined;
+                        if (other.path.length > op.path.length) {
+                            other.op = op.op;
+                            other.path = op.path;
+                            delete other.value;
+                        } else {
+                            removeOps.push(other);
+                        }
+                    }
                 } else {
                     for (const [index, key] of op.path.entries()) {
                         if (index > other.path.length) break;
@@ -46,7 +175,17 @@ export function mergePatch(patch) {
                     }
                 }
             } else if (op.op === 'replace') {
-                if (other.op === 'add' && op.path.join('/') === other.path.join('/')) {
+                console.assert(false)
+                if (other.op === 'add' && isPrefix(other.path, op.path)) {
+                    // Case 3) add A + replace B -> add B
+                    other.value = op.value;
+                    if (other.path.length > op.path.length) {
+                        other.op = 'replace';
+                        other.path = op.path;
+                    }
+                    addOp = undefined;
+                } else if (other.op === 'replace' && op.path.join('/') === other.path.join('/')) {
+                    // Case 1) replace A + replace B -> replace B
                     other.value = op.value;
                     addOp = undefined;
                 }
@@ -56,19 +195,20 @@ export function mergePatch(patch) {
         }
 
         for (const removeOp of removeOps) {
-            const index = normalizedPatch.indexOf(removeOp);
+            const index = mergedPatch.indexOf(removeOp);
             console.assert(index !== -1)
-            normalizedPatch.splice(index, 1);
+            mergedPatch.splice(index, 1);
         }
 
         if (addOp) {
-            normalizedPatch.push(addOp);
+            mergedPatch.push(addOp);
         }
     }
 
-    const normalizedPatch = [];
-    for (let op of patch) {
-        normalizeOperation(Object.assign({}, op));
+    for (let op of normalizedPatch) {
+        mergeOperation(Object.assign({}, op));
     }
-    return normalizedPatch
+
+    mergedPatch.forEach(op => op.path = op.path.join('/'))
+    return mergedPatch
 }

f0f22da25d92f3ac7cc76afebb372aff530c09d0
Wolfgang Kühn <wolfgang-kuehn@decatur.de>
2020-09-08 00:30:46 +0200

    intro stage 2

--- gridchen/tests/test_json_patch_merge.js
+++ gridchen/tests/test_json_patch_merge.js
@@ -1,41 +1,114 @@
 import {test, assert} from '../testing/utils.js'
-import {mergePatch} from "../json_patch_merge.js";
+import {dispense} from "../json_patch_merge.js";
 
 
-test('normalize1', () => {
+test('replace A + replace B -> replace B', () => {
+    const patch = [
+        {"op": "replace", "path": "/a/b", "value": "A"},
+        {"op": "replace", "path": "/a/b", "value": "B"}
+    ];
+    const mergedPatch = dispense(patch);
+    assert.equal([{"op": "replace", "path": "/a/b", "value": "B"}], mergedPatch);
+});
+
+test('replace A + remove -> remove', () => {
+    const patch = [
+        {"op": "replace", "path": "/a/b", "value": "A"},
+        {"op": "remove", "path": "/a/b"}
+    ];
+    const mergedPatch = dispense(patch);
+    assert.equal([{"op": "remove", "path": "/a/b"}], mergedPatch);
+});
+
+test('add A + replace B -> add B', () => {
+    let patch = [
+        {"op": "add", "path": "/a/b", "value": "A"},
+        {"op": "replace", "path": "/a/b", "value": "B"}
+    ];
+    let mergedPatch = dispense(patch);
+    assert.equal([{"op": "add", "path": "/a/b", "value": "B"}], mergedPatch);
+
+    patch = [
+        {"op": "add", "path": "/a/b", "value": "A"},
+        {"op": "replace", "path": "/a", "value": "B"}
+    ];
+    mergedPatch = dispense(patch);
+    assert.equal([{"op": "replace", "path": "/a", "value": "B"}], mergedPatch);
+});
+
+test('add A + remove -> NoOp', () => {
+    const patch = [
+        {"op": "add", "path": "/a/b", "value": "A"},
+        {"op": "remove", "path": "/a/b"}
+    ];
+    const mergedPatch = dispense(patch);
+    assert.equal([], mergedPatch);
+});
+
+test('(indexed) replace A + replace B -> replace B', () => {
+    const patch = [
+        {"op": "replace", "path": "/a/1", "value": "A"},
+        {"op": "replace", "path": "/a/1", "value": "B"}
+    ];
+    const mergedPatch = dispense(patch);
+    assert.equal([{"op": "replace", "path": "/a/1", "value": "B"}], mergedPatch);
+});
+
+test('(indexed) replace A + remove -> remove', () => {
+    let patch = [
+        {"op": "replace", "path": "/a/1", "value": "A"},
+        {"op": "remove", "path": "/a/1"}
+    ];
+    let mergedPatch = dispense(patch);
+    assert.equal([{"op": "remove", "path": "/a/1"}], mergedPatch);
+
+    patch = [
+        {"op": "replace", "path": "/a/1", "value": "A"},
+        {"op": "add", "path": "/a/2", "value": "B"},
+        {"op": "remove", "path": "/a/1"}
+    ];
+    mergedPatch = dispense(patch);
+    assert.equal([
+        {"op":"add","path":"/a/2","value":null},
+        {"op":"remove","path":"/a/1"},
+        {"op":"replace","path":"/a/1","value":"B"}], mergedPatch);
+});
+
+test('(indexed) add A + replace B -> add B', () => {
+    const patch = [
+        {"op": "add", "path": "/a/1", "value": "A"},
+        {"op": "replace", "path": "/a/1", "value": "B"}
+    ];
+    const mergedPatch = dispense(patch);
+    assert.equal([{"op": "add", "path": "/a/1", "value": null}, {"op": "replace", "path": "/a/1", "value": "B"}], mergedPatch);
+});
+
+test('(indexed) add A + remove -> NoOp', () => {
+    const patch = [
+        {"op": "add", "path": "/a/1", "value": "A"},
+        {"op": "remove", "path": "/a/1"}
+    ];
+    const mergedPatch = dispense(patch);
+    assert.equal([], mergedPatch);
+});
+
+test('(indexed) add A + remove -> NoOp', () => {
     const patch = [
         {"op": "add", "path": "/a/1", "value": 1},
         {"op": "add", "path": "/a/1", "value": 2},
         {"op": "remove", "path": "/a/2"}
     ];
-    const mergedPatch = mergePatch(patch);
-    assert.equal([{"op": "add", "path": ["", "a", 1], "value": 2}], mergedPatch);
+    const mergedPatch = dispense(patch);
+    const expected = [{"op":"add","path":"/a/1","value":null},{"op":"add","path":"/a/1","value":null},{"op":"remove","path":"/a/2"},{"op":"replace","path":"/a/1","value":2}];
+    assert.equal(expected, mergedPatch);
 });
 
-test('normalize2', () => {
-    const patch = [
-        {"op": "add", "path": "/a", "value": 1},
-        {"op": "add", "path": "/b", "value": 2},
-        {"op": "remove", "path": "/a"}
-    ];
-    const mergedPatch = mergePatch(patch);
-    assert.equal([{"op": "add", "path": ["", "b"], "value": 2}], mergedPatch);
-});
 
-test('normalize3', () => {
-    const patch = [
-        {"op": "add", "path": "/a", "value": 1},
-        {"op": "replace", "path": "/a", "value": 2}
-    ];
-    const mergedPatch = mergePatch(patch);
-    assert.equal([{"op": "add", "path": ["", "a"], "value": 2}], mergedPatch);
-});
-
-test('normalize4', () => {
+test('prefix', () => {
     const patch = [
         {"op": "add", "path": "/a/1", "value": 1},
         {"op": "remove", "path": "/a"}
     ];
-    const mergedPatch = mergePatch(patch);
-    assert.equal(0, mergedPatch.length);
+    const mergedPatch = dispense(patch);
+    assert.equal([{"op": "remove", "path": "/a"}], mergedPatch);
 });
